4cde1eace67808ea4d05d1941328cae32117a986
Aashish Upadhyay <aupadhyay@gmail.com>
2021-04-14 22:58:12 -0400

    ghost.py

---
+++ src/ghost.py
@@ -0,0 +1,26 @@
+from src.trie import Trie
+
+class Ghost:
+    def findWinningLetters(self, words):
+        result =[]
+        d = Trie(words)
+
+        ks = d.trie.keys()
+        for k in ks:
+            if self.isWinning(k, d):
+                result.append(k)
+
+        return result        
+
+    def isWinning(self, prefix, trie):
+        d = trie.find(prefix)
+
+        if '#' in d:
+            return False
+        else:
+            ks = d.keys()
+            for k in ks:
+                if self.isWinning(prefix+k, trie) == False:
+                    return True
+
+            return False        
---
+++ testghost.py
@@ -0,0 +1,17 @@
+from src.ghost import Ghost
+
+def test1():
+    g = Ghost()
+    assert set(['b', 'c']) == set(g.findWinningLetters(["cat", "calf","bear","dog","ca"]))
+
+def test2():
+    g = Ghost()
+    assert set(['c']) == set(g.findWinningLetters(["ca"]))
+
+def test3():
+    g = Ghost()
+    assert set(['b']) == set(g.findWinningLetters(["bear"]))
+
+def test4():
+    g = Ghost()
+    assert set([]) == set(g.findWinningLetters(["bear","bea"]))
