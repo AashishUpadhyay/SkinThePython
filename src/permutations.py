d1ff23134c4e6c3986d58eaab7988f34c85e7f01
Aashish Upadhyay <aupadhyay@gmail.com>
2021-04-16 16:05:54 -0400

    permutations.py

---
+++ src/permutations.py
@@ -0,0 +1,24 @@
+class Permutations:
+    def __init__(self, arr):
+        self._arr = arr
+        self._perms =[]
+
+    def generatePermutations(self):
+        self._generatePermutationsInternal(self._arr, "", set())
+        return self._perms
+
+    def _generatePermutationsInternal(self, arr, perm, seen):
+
+        if len(arr) == len(seen):
+            self._perms.append(perm)
+
+        for i in range(len(arr)):
+            if i in seen:
+                continue
+
+            seen.add(i)
+            self._generatePermutationsInternal(arr, perm + arr[i], seen)
+            seen.remove(i)    
+
+
+
