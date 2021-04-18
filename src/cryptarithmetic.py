42bb449139a34b3c560d50767b0766c277a4b74d
Aashish Upadhyay <aupadhyay@gmail.com>
2021-04-18 17:48:58 -0400

    cryptarithmetic

---
+++ src/cryptarithmetic.py
@@ -0,0 +1,77 @@
+import copy
+from collections import OrderedDict
+
+class Cryptarithmetic:
+    def __init__(self, problem):
+        self.words = problem
+        self.result = []
+
+    def solve(self):
+        wordsArr = []
+        for w in self.words:
+            wordArr = []
+            for c in w:
+                wordArr.append(c)
+            wordsArr.append(wordArr)  
+
+        self.normalizeWords(wordsArr)
+        
+        letters = OrderedDict()
+        maxLen = len(self.words[2])
+
+        for i in range(maxLen-1, -1, -1):
+            for w in wordsArr:
+                if w[i] not in letters:
+                    letters[w[i]] = None
+
+        unassigned = [key for key in letters.keys() if key != '#']
+        letters['#'] = 0
+        self.solveInternal(letters, unassigned, wordsArr, set())
+        return self.result
+
+    def solveInternal(self, letters, unassigned, wordsArr, seen):
+        if not unassigned:
+            if self.isValid(wordsArr, letters):
+                self.result.append(copy.deepcopy(letters))
+            return
+
+        c = unassigned[0]
+        for i in range(10):
+            if i in seen:
+                continue
+            
+            letters[c] = i
+            seen.add(i)  
+
+            if self.isValid(wordsArr, letters):
+                self.solveInternal(letters, unassigned[1:], wordsArr, seen)
+
+            seen.remove(i)    
+            letters[c] = None
+        
+        return None         
+
+    def isValid(self, wordsArr, letters):
+        a,b,c = wordsArr
+        n = len(wordsArr[0])
+        carry = 0
+        for i in range(n-1,-1,-1):
+            if any(letters[w[i]] == None for w in wordsArr if w[i] != '#'):
+                return True
+            elif (letters[a[i]] + letters[b[i]] + carry) == letters[c[i]]:
+                carry = 0
+            elif (letters[a[i]] + letters[b[i]] + carry) == (10 + letters[c[i]]):
+                carry = 1
+            else:
+                return False
+
+        return True                
+
+    def normalizeWords(self, wordsArr):
+        maxLen = len(wordsArr[2])    
+
+        for w in wordsArr:
+            ct = maxLen - len(w)
+            while ct > 0:
+                w.insert(0,'#')
+                ct = ct-1   
---
+++ testcryptarithmetic.py
@@ -0,0 +1,234 @@
+from src.cryptarithmetic import Cryptarithmetic
+from collections import OrderedDict
+import sys
+
+def testisvalid1():
+    words = ['SEND','MORE', 'MONEY']
+    crypt = Cryptarithmetic(words)
+    # words = ['9567','1O85', '1O652']
+    
+    wordsArr = []
+    letters = {}
+
+    for w in words:
+        for c in w:
+            if c not in letters:
+                letters[c] = None
+
+    letters['S'] = None
+    letters['E'] = None
+    letters['N'] = None
+    letters['D'] = 7
+    letters['M'] = None
+    letters['O'] = None
+    letters['R'] = None
+    letters['Y'] = None
+    letters['#'] = 0
+
+    for w in words:
+        wordArr = []
+        for c in w:
+            wordArr.append(c)    
+        wordsArr.append(wordArr)
+
+    crypt.normalizeWords(wordsArr)
+    assert True == crypt.isValid(wordsArr, letters)
+
+def testisvalid2():
+    words = ['SEND','MORE', 'MONEY']
+    crypt = Cryptarithmetic(words)
+    # words = ['9567','1O85', '1O652']
+    
+    wordsArr = []
+    letters = {}
+
+    for w in words:
+        for c in w:
+            if c not in letters:
+                letters[c] = None
+
+    letters['S'] = None
+    letters['E'] = 5
+    letters['N'] = None
+    letters['D'] = 7
+    letters['M'] = None
+    letters['O'] = None
+    letters['R'] = None
+    letters['Y'] = None
+    letters['#'] = 0
+
+    for w in words:
+        wordArr = []
+        for c in w:
+            wordArr.append(c)    
+        wordsArr.append(wordArr)
+
+    crypt.normalizeWords(wordsArr)
+    assert True == crypt.isValid(wordsArr, letters)
+
+def testisvalid3():
+    words = ['SEND','MORE', 'MONEY']
+    crypt = Cryptarithmetic(words)
+    # words = ['9567','1O85', '1O652']
+    
+    wordsArr = []
+    letters = {}
+
+    for w in words:
+        for c in w:
+            if c not in letters:
+                letters[c] = None
+
+    letters['S'] = None
+    letters['E'] = 5
+    letters['N'] = None
+    letters['D'] = 7
+    letters['M'] = None
+    letters['O'] = None
+    letters['R'] = None
+    letters['Y'] = 2
+    letters['#'] = 0
+
+    for w in words:
+        wordArr = []
+        for c in w:
+            wordArr.append(c)    
+        wordsArr.append(wordArr)
+
+    crypt.normalizeWords(wordsArr)
+    assert True == crypt.isValid(wordsArr, letters)
+
+def testisvalid4():
+    words = ['SEND','MORE', 'MONEY']
+    crypt = Cryptarithmetic(words)
+    # words = ['9567','1O85', '1O652']
+    
+    wordsArr = []
+    letters = {}
+
+    for w in words:
+        for c in w:
+            if c not in letters:
+                letters[c] = None
+
+    letters['S'] = None
+    letters['E'] = 5
+    letters['N'] = None
+    letters['D'] = 7
+    letters['M'] = None
+    letters['O'] = None
+    letters['R'] = None
+    letters['Y'] = 3
+    letters['#'] = 0
+
+    for w in words:
+        wordArr = []
+        for c in w:
+            wordArr.append(c)    
+        wordsArr.append(wordArr)
+
+    crypt.normalizeWords(wordsArr)
+    assert False == crypt.isValid(wordsArr, letters)
+
+def testisvalid5():
+    words = ['SEND','MORE', 'MONEY']
+    crypt = Cryptarithmetic(words)
+    # words = ['9567','1O85', '1O652']
+    
+    wordsArr = []
+    letters = {}
+
+    for w in words:
+        for c in w:
+            if c not in letters:
+                letters[c] = None
+
+    letters['S'] = 9
+    letters['E'] = 5
+    letters['N'] = 6
+    letters['D'] = 7
+    letters['M'] = 1
+    letters['O'] = 0
+    letters['R'] = 8
+    letters['Y'] = 2
+    letters['#'] = 0
+
+    for w in words:
+        wordArr = []
+        for c in w:
+            wordArr.append(c)    
+        wordsArr.append(wordArr)
+
+    crypt.normalizeWords(wordsArr)
+    assert True == crypt.isValid(wordsArr, letters)
+
+def testNormalizeWords():
+    crypt = Cryptarithmetic([])
+    words = [['S','E','N','D'],['M','O','R','E'],['M','O','N','E','Y']]
+    crypt.normalizeWords(words)
+    assert words[0][0] == '#'
+    assert words[1][0] == '#'
+
+def test1():
+    words = ['B','AC', 'AD']
+    crypt = Cryptarithmetic(words)
+    result = crypt.solve()
+    found = False
+    for r in result:
+        if r['B'] == 8 and r['C'] == 1 and r['D'] == 9 and r['A'] == 7:
+            found = True
+            break
+
+    assert found    
+
+def test2():
+    sys.setrecursionlimit(1500)
+    words = ['SEND','MORE', 'MONEY']
+    crypt = Cryptarithmetic(words)
+    result = crypt.solve()
+    found = False
+    for r in result:
+        if r['D'] == 7 and r['E'] == 5 and r['Y'] == 2 and r['N'] == 6 and r['R']== 8 and r['O'] == 0 and r['S'] == 9 and r['M'] == 1:
+            found = True
+ 
+    assert found
+
+def testisvalid6():
+    letters = OrderedDict([('S', 0), ('E', 1), ('N', 2), ('D', None), ('M', None), ('O', None), ('R', None), ('Y', None)])
+    
+    wordsArr = [['#', 'S', 'E', 'N', 'D'], ['#', 'M', 'O', 'R', 'E'], ['M', 'O', 'N', 'E', 'Y']]
+    crypt = Cryptarithmetic([])
+    crypt.normalizeWords(wordsArr)
+    letters = {}
+    maxLen = len(wordsArr[2])
+
+    for i in range(maxLen-1, -1, -1):
+        for w in wordsArr:
+            if w[i] not in letters:
+                letters[w[i]] = None
+
+    letters['D'] = 0
+    letters['E'] = 1
+    letters['Y'] = 2
+
+    assert False == crypt.isValid(wordsArr, letters)
+
+def testisvalid7():
+    letters = OrderedDict([('S', 0), ('E', 1), ('N', 2), ('D', None), ('M', None), ('O', None), ('R', None), ('Y', None)])
+    
+    wordsArr = [['#', 'S', 'E', 'N', 'D'], ['#', 'M', 'O', 'R', 'E'], ['M', 'O', 'N', 'E', 'Y']]
+    crypt = Cryptarithmetic([])
+    crypt.normalizeWords(wordsArr)
+    letters = {}
+    maxLen = len(wordsArr[2])
+
+    for i in range(maxLen-1, -1, -1):
+        for w in wordsArr:
+            if w[i] not in letters:
+                letters[w[i]] = None
+
+    letters['D'] = 1
+    letters['E'] = 2
+    letters['Y'] = 3
+
+    assert True == crypt.isValid(wordsArr, letters)
