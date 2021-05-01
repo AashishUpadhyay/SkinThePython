fbcf77d9cbf75ab7f7c4200c1421ed3716c20a4f
Aashish Upadhyay <aupadhyay@gmail.com>
2021-05-01 14:10:02 -0400

    implemented fixedsizequeue

--- src/fixedsizequeue.py
+++ src/fixedsizequeue.py
@@ -4,6 +4,7 @@ class FixedSizeQueue:
         self._tail = 0
         self._items = [None] * size
         self._limit = size
+        self._size = 0
 
     def enqueue(self, item):
         if self.qsize() == self._limit:
@@ -15,6 +16,8 @@ class FixedSizeQueue:
             self._tail = 0
         else:
             self._tail += 1
+
+        self._size +=1    
         return 1    
 
     def dequeue(self):
@@ -29,18 +32,9 @@ class FixedSizeQueue:
         else:
             self._head += 1
 
+        self._size -=1    
+
         return result
 
     def qsize(self):
-
-        if self._tail > self._head:
-            return self._tail - self._head
-        elif self._tail < self._head:  
-            return (self._tail - 0) + (self._limit - self._head)
-        else:
-            if self._head != (self._limit - 1) and self._items[self._head+1] != None:
-                return self._limit
-            elif self._tail != 0 and self._items[self._tail-1] != None:     
-                return self._limit
-            else:
-                return 0    
+        return self._size
