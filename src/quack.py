cea21c95168e208f9a706acc4169465c70a22b15
Aashish Upadhyay <aupadhyay@gmail.com>
2021-05-01 17:39:56 -0400

    implemented quack

---
+++ src/quack.py
@@ -0,0 +1,48 @@
+class Quack:
+    def __init__(self):
+        self._left = []
+        self._right = []
+        self._buffer = []
+        pass
+
+    def push(self, item):
+        self._left.append(item)
+
+    def pop(self):
+
+        if not self._left and not self._right:
+            return -1
+
+        if not self._left:
+
+            rt_len = len(self._right)
+
+            for _ in range(rt_len//2):
+                self._buffer.append(self._right.pop())
+
+            while self._right:
+                self._left.append(self._right.pop())    
+
+            while self._buffer:
+                self._right.append(self._buffer.pop())  
+
+        return self._left.pop()
+
+    def pull(self):
+        if not self._left and not self._right:
+            return -1
+
+        if not self._right:
+
+            lf_len = len(self._left)
+
+            for _ in range(lf_len//2):
+                self._buffer.append(self._left.pop())
+
+            while self._left:
+                self._right.append(self._left.pop())    
+
+            while self._buffer:
+                self._left.append(self._buffer.pop())  
+
+        return self._right.pop()
---
+++ tests/test_quack.py
@@ -0,0 +1,27 @@
+from src.quack import Quack
+
+def test_1():
+    quack = Quack()
+
+    quack.push(1)
+    quack.push(2)
+    quack.push(3)
+    quack.push(4)
+    quack.push(5)
+    quack.push(6)
+
+    assert quack.pop() == 6
+    assert quack.pull() == 1
+    assert quack.pull() == 2
+
+    quack.push(7)
+    quack.push(8)
+
+    assert quack.pop() == 8
+    assert quack.pop() == 7
+
+    assert quack.pull() == 3
+    assert quack.pull() == 4
+    assert quack.pull() == 5
+    assert quack.pull() == -1
+    assert quack.pop() == -1
