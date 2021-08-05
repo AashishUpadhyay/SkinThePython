fe58701d536dcf87b6c516cb6890ccc38543412f
Aashish Upadhyay <aashishupadhyay@Aashishs-MacBook-Pro.local>
2021-08-04 23:37:31 -0400

    tsp optimized

--- src/tsp.py
+++ src/tsp.py
@@ -1,6 +1,7 @@
 from sys import path
 import copy
 
+
 class TravelingSalesmanProblem:
     def __init__(self) -> None:
         pass
@@ -13,54 +14,51 @@ class TravelingSalesmanProblem:
         for i, row in enumerate(graph):
             adj_list[i] = set(row)
             self.nodes.append(i)
-            
+
         if len(self.nodes) == 1:
-            return 0     
+            return 0
 
         self.max_len = len(self.nodes) * (len(self.nodes) - 1)
         result = self.max_len
 
         for k in self.nodes:
             path_visited = set()
-            nodes_visited = set()
-            result = min(self.dfs(k, adj_list, path_visited, nodes_visited), result)
+            nodes_visited = f'{0:012b}'
+            result = min(
+                self.dfs(k, adj_list, path_visited, nodes_visited), result)
 
-        return result - 1 
-    
+        return result - 1
+
     def dfs(self, node, graph, path_visited, nodes_visited):
 
-        if len(nodes_visited) == len(self.nodes):
+        if int(nodes_visited, 2) == (pow(2, len(self.nodes)) - 1):
             return 0
 
         nbours = graph[node]
+        nodes_visited_as_int = int(nodes_visited, 2)
 
-        key_arr = []
-        key_arr.extend([str(ka) for ka in self.nodes if ka in nodes_visited]) 
-        key_arr = [node]
-        key_arr.extend([str(ka) for ka in self.nodes if ka not in nodes_visited ]) 
-        key_str = "-".join([str(ka) for ka in key_arr])
+        new_nodes_visited = nodes_visited_as_int | (1 << node)
+        key_str = f'{new_nodes_visited:012b}'
 
-        nodes_visited.add(node)
-        
-        if key_str in self.dp:
-            return self.dp[key_str]
+        if node not in self.dp:
+            self.dp[node] = {}
+
+        if key_str in self.dp[node]:
+            return self.dp[node][key_str]
 
         ret_val = self.max_len
         for nb in nbours:
             path = str(node) + "-" + str(nb)
             rec_result = None
-            if path not in path_visited:
-                path_visited.add(path)
-                graph[node].remove(nb)
-                rec_result = self.dfs(nb, graph, path_visited, copy.deepcopy(nodes_visited))
-                path_visited.remove(path)
-                graph[node].add(nb)
+            if path in path_visited:
+                continue
 
-                if rec_result == -1:
-                    continue
+            path_visited.add(path)
+            rec_result = self.dfs(
+                nb, graph, path_visited, key_str)
+            path_visited.remove(path)
 
-            ret_val = min(ret_val, (rec_result+1))   
-        
-        self.dp[key_str] = ret_val
-        return ret_val   
+            ret_val = min(ret_val, (rec_result+1))
 
+        self.dp[node][key_str] = ret_val
+        return ret_val
