b52e18a255ca2e1e9bcff06713924e5b89d83a61
aashish <aashish@example.com>
2021-10-21 13:04:29 -0400

    longest_palindrome_subsequence

---
+++ tests/test_longest_palindrome_subsequence.py
@@ -0,0 +1,17 @@
+from src.longest_palindrome_subsequence import LongestPalindromeSubsequence
+
+
+def test1():
+    lps = LongestPalindromeSubsequence()
+    assert lps.find('agbdba') == 5
+
+def test2():
+    lps = LongestPalindromeSubsequence()
+    assert lps.find('agbddba') == 6
+
+def test3():
+    lps = LongestPalindromeSubsequence()
+    assert lps.find('bbbab') == 4
+    
+
+
